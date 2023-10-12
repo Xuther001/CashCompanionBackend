d4c513bd165159e4d5affcc82603ace38dfdd776
Will <wt@Wills-MacBook-Pro.local>
2023-10-12 04:15:04 -0700

    added @GetMapping to get all notesfrom an endpoint

--- src/main/java/com/app/chat/notes/NoteController.java
+++ src/main/java/com/app/chat/notes/NoteController.java
@@ -3,6 +3,8 @@ package com.app.chat.notes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/notes")
 @CrossOrigin(origins = "/api/v1/notes")
@@ -14,5 +16,10 @@ public class NoteController {
     @PostMapping
     public Note addNote(@RequestBody Note note) {
         return noteRepository.save(note);
+    }
+
+    @GetMapping
+    public List<Note> getAllNotes() {
+        return noteRepository.findAll();
     }
 }
